2bc9ecbdcf3b7fc6199b418bdbda0e2f413c656e
Nikolay Syritsky <68183725+mrhabpwnz@users.noreply.github.com>
2021-04-30 21:25:38 +0300

    Meditation app with different options of audio and video.

---
+++ app.js
@@ -0,0 +1,75 @@
+const song = document.querySelector(".song"),
+play = document.querySelector(".play"),
+replay = document.querySelector(".replay"),
+outline = document.querySelector(".moving-outline circle"),
+video = document.querySelector(".vid-container video"),
+sounds = document.querySelectorAll(".sound-picker button"),
+timeDisplay = document.querySelector(".time-display"),
+timeSelect = document.querySelectorAll(".time-select button"),
+outlineLength = outline.getTotalLength();
+let fakeDuration = 600;
+
+outline.style.strokeDashoffset = outlineLength;
+outline.style.strokeDasharray = outlineLength;
+timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+  fakeDuration % 60
+)}`;
+
+const checkPlaying = song => {
+  if (song.paused) {
+    song.play();
+    video.play();
+    play.src = "./svg/pause.svg";
+  } else {
+    song.pause();
+    video.pause();
+    play.src = "./svg/play.svg";
+  }
+};
+
+const restartSong = song =>{
+  let currentTime = song.currentTime;
+  song.currentTime = 0;
+}
+
+sounds.forEach(sound => {
+  sound.addEventListener("click", function() {
+    song.src = this.getAttribute("data-sound");
+    video.src = this.getAttribute("data-video");
+    checkPlaying(song);
+  });
+});
+
+play.addEventListener("click", function() {
+  checkPlaying(song);
+});
+
+replay.addEventListener("click", function() {
+    restartSong(song);
+  });
+
+timeSelect.forEach(option => {
+  option.addEventListener("click", function() {
+    fakeDuration = this.getAttribute("data-time");
+    timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+      fakeDuration % 60
+    )}`;
+  });
+});
+
+song.ontimeupdate = function() {
+  let currentTime = song.currentTime;
+  let elapsed = fakeDuration - currentTime;
+  let seconds = Math.floor(elapsed % 60);
+  let minutes = Math.floor(elapsed / 60);
+  timeDisplay.textContent = `${minutes}:${seconds}`;
+  let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+  outline.style.strokeDashoffset = progress;
+
+  if (currentTime >= fakeDuration) {
+    song.pause();
+    song.currentTime = 0;
+    play.src = "./svg/play.svg";
+    video.pause();
+  }
+};
